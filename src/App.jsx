d1a63da633a7d05d755273fe8334438e8a787068
Coker <gbolahancoker99@gmail.com>
2024-05-12 22:39:59 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import HomeScreen from './components/HomeScreen/HomeScreen'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/Inputs/Login'
 const App = () => {
-
-  
+  const user = null
   return (
-    <div  className='app'>
-      <HomeScreen />
-      {/* <Nav /> */}
-      {/* <Banner /> */}
-      
-       {/* <MovieRow title="Horror Movies" fetchURL={request.fetchAll}/> */}
-      
+    <div className='app'>
+      <BrowserRouter >
+      <Routes>
+        {!user ?(
+         <Route path='/'  element={<Login />} />
+        ):(
+          <Route path="/home" element={<HomeScreen />} />
+        )
+        }
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
